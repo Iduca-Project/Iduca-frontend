236524b1d66b13f99baf856de079e91ec0960d2b
sasa2754 <sasamor5@gmail.com>
2025-05-26 15:15:43 -0300

    fiz o editProfile #19

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Menu } from "@/src/components/menu";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import pessoa from "../../../public/image/pessoa.jpg";
 import { Autocomplete, Box, Chip, TextField } from "@mui/material";
 import { CuteButton } from "@/src/components/cuteButton";
@@ -10,6 +10,8 @@ import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import { Card } from "@/src/components/card";
 import { useRouter } from 'next/navigation'
+import { useRef, useState } from "react";
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 
 
 const interests = [
@@ -22,7 +24,23 @@ const interests = [
 
 
 const Profile = () => {
+    const [editOn, setEditOn] = useState(false);
     const router = useRouter();
+    const [preview, setPreview] = useState<StaticImageData | string>(pessoa);
+    const fileInputRef = useRef<HTMLInputElement>(null);
+  
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0];
+      if (file) {
+        const imageURL = URL.createObjectURL(file);;
+        setPreview(imageURL);
+        // Aqui você pode mandar o file pro backend via form ou state
+      }
+    };
+  
+    const handleClick = () => {
+      fileInputRef.current?.click();
+    };
     
     const goToCertificate = (id : number) => {
         router.push(`/certificate/${id}`)
@@ -41,42 +59,102 @@ const Profile = () => {
                 {/* Profile info */}
                 <div className="grid xl:grid-cols-[1.1fr_1.5fr] grid-cols-1 w-full gap-6">
                     <div className="bg-(--card)  border border-(--stroke) shadow-(--shadow) rounded-2xl px-6 py-10 flex flex-col gap-20 items-center">
-                        <Image className="rounded-full object-cover aspect-square w-52" src={pessoa} alt="imagem" width={3000} height={3000} priority></Image>
-                        <div className="flex flex-col gap-6 text-(--text)">
-                            <TextField label="Nome completo" variant="outlined" defaultValue={`Sabrina Mortean`} slotProps={{ input: { readOnly: true } }}/>
-                            <TextField label="E-mail corporativo" variant="outlined" defaultValue={`sabrina@bosch.com`} slotProps={{ input: { readOnly: true } }} sx={{ color: "inherit" }}/>
-                            <Autocomplete
-                                multiple
-                                readOnly
-                                disableClearable
-                                options={interests.map((item) => item.title)}
-                                defaultValue={interests.map((item) => item.title)}
-                                disableCloseOnSelect
-                                sx={{ color: "var(--text)" }}
-                                renderInput={(params) => (
-                                    <TextField {...params} label="Interesses" variant="outlined" sx={{ color: "inherit" }}/>
-                                )}
-                                renderValue={(selected) => (
-                                    <div style={{ display: 'flex', flexWrap: 'wrap', gap: 8 }}>
-                                        {selected.map((option, index) => (
-                                            <Chip
-                                            key={index}
-                                            label={option}
-                                            sx={{
-                                                backgroundColor: 'var(--aquamarine)', 
-                                                color: 'white',
-                                                fontWeight: 'bold'
+                            {!editOn ? (
+                                <>
+                                    <Image className="rounded-full object-cover aspect-square w-52" src={pessoa} alt="imagem" width={3000} height={3000} priority></Image>
+                                    <div className="flex flex-col gap-6 text-(--text)">
+                                        <TextField label="Nome completo" variant="outlined" defaultValue={`Sabrina Mortean`} slotProps={{ input: { readOnly: true } }}/>
+                                        <TextField label="E-mail corporativo" variant="outlined" defaultValue={`sabrina@bosch.com`} slotProps={{ input: { readOnly: true } }} sx={{ color: "inherit" }}/>
+                                        <Autocomplete
+                                            multiple
+                                            readOnly
+                                            disableClearable
+                                            options={interests.map((item) => item.title)}
+                                            defaultValue={interests.map((item) => item.title)}
+                                            disableCloseOnSelect
+                                            sx={{ color: "var(--text)" }}
+                                            renderInput={(params) => (
+                                                <TextField {...params} label="Interesses" variant="outlined" sx={{ color: "inherit" }}/>
+                                            )}
+                                            renderValue={(selected) => (
+                                                <div style={{ display: 'flex', flexWrap: 'wrap', gap: 8 }}>
+                                                    {selected.map((option, index) => (
+                                                        <Chip
+                                                        key={index}
+                                                        label={option}
+                                                        sx={{
+                                                            backgroundColor: 'var(--aquamarine)', 
+                                                            color: 'white',
+                                                            fontWeight: 'bold'
+                                                        }}
+                                                        />
+                                                    ))}
+                                                </div>
+                                            )}
+                                            slotProps={{
+                                                popupIndicator: { style: { display: 'none' } }
                                             }}
-                                            />
-                                        ))}
+                                        />
                                     </div>
-                                )}
-                                slotProps={{
-                                    popupIndicator: { style: { display: 'none' } }
-                                }}
-                            />
-                        </div>
-                        <CuteButton text="Editar Perfil" icon={ArrowForwardIcon}></CuteButton>
+                                    <CuteButton text="Editar Perfil" onClick={() => setEditOn(true)} icon={ArrowForwardIcon}></CuteButton>
+
+                                </>
+                            ) : (
+                                <>
+                                    <div className="relative w-52 aspect-square cursor-pointer flex items-center justify-center" onClick={handleClick}>
+                                        <div className="absolute inset-0 bg-black opacity-50 rounded-full flex items-center justify-center">
+                                            <EditOutlinedIcon className="text-white" fontSize="large" />
+                                        </div>
+                                            
+                                            <Image
+                                                className="rounded-full object-cover aspect-square"
+                                                src={preview}
+                                                alt="Foto de perfil"
+                                                width={208} height={208}
+                                            />
+
+                                            <input
+                                                type="file"
+                                                accept="image/*"
+                                                ref={fileInputRef}
+                                                onChange={handleFileChange}
+                                                className="hidden"
+                                            />
+                                    </div>
+                                    <div className="flex flex-col gap-6 text-(--text)">
+                                        <TextField label="Nome completo" variant="outlined" defaultValue={`Sabrina Mortean`}/>
+                                        <TextField label="E-mail corporativo" variant="outlined" defaultValue={`sabrina@bosch.com`} sx={{ color: "inherit" }}/>
+                                        <Autocomplete
+                                            multiple
+                                            disableClearable
+                                            options={interests.map((item) => item.title)}
+                                            defaultValue={interests.map((item) => item.title)}
+                                            disableCloseOnSelect
+                                            sx={{ color: "var(--text)", width: "100%" }}
+                                            renderInput={(params) => (
+                                                <TextField {...params} label="Interesses" variant="outlined" sx={{ color: "inherit" }}/>
+                                            )}
+                                            renderValue={(selected) => (
+                                                <div style={{ display: 'flex', flexWrap: 'wrap', gap: 8 }}>
+                                                    {selected.map((option, index) => (
+                                                        <Chip
+                                                        key={index}
+                                                        label={option}
+                                                        sx={{
+                                                            backgroundColor: 'var(--aquamarine)', 
+                                                            color: 'white',
+                                                            fontWeight: 'bold'
+                                                        }}
+                                                        />
+                                                    ))}
+                                                </div>
+                                            )}
+                                        />
+                                    </div>
+                                    <CuteButton text="Editar Perfil" onClick={() => setEditOn(false)} icon={ArrowForwardIcon}></CuteButton>
+                                </>
+                            )}
+                            
                     </div>   
 
                     {/* Courses */}
